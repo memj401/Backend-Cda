6f31c14ec8a3cc5cda6033dbdb9f70f157247418
4000napa <beto.lhcs@gmail.com>
2020-05-18 12:27:17 -0300

    documentação da controladora rfid pronta

--- src/controladoras/pyControladora.js
+++ src/controladoras/pyControladora.js
@@ -6,7 +6,7 @@ const rfidRepositorio = require('../repositorios/rfid')
 */
 const pyControladora = {
     /**
-        * Lida com a requisição post recebendo o valor do rfid e colocando no banco de dados
+        * Lida com a requisição POST recebendo o valor do rfid e colocando no banco de dados
         * @memberof pyControladora
         * @method receber
         * @param {Object} requisicao Parametro padrão e fornecido pelo Express, guarda as informações da requisição como corpo e o tipo
@@ -23,6 +23,14 @@ const pyControladora = {
         rfidRepositorio.inserir(informaçõesDaEntrada)
         return resposta.status(200).end()
     },
+    /**
+        * Lida com requisição GET respondendo com a entrada mais recente do banco de dados
+        * @memberof pyControladora
+        * @method buscarUltimo
+        * @param {Object} requisicao Parametro padrão e fornecido pelo Express, guarda as informações da requisição como corpo e o tipo
+        * @param {Object} resposta Parametro padrão e fornecido pelo Express, guarda as informações da resposta como o corpo e o status
+        * @returns O retorno nessa função é desnecessário e é feito só para não gerar confusão quanto ao fim da função, o que importa é a chamada dos metodos do obejto "resposta"
+    */
     buscarUltimo: async function (requisicao, resposta){
         const dados = await rfidRepositorio.buscarUltima()
         if (dados.valido === true) { //[1] é a coluna da validação, se o RFID for valido ou seja igual a true n pode cadastrar ele denovo
@@ -32,6 +40,14 @@ const pyControladora = {
             return resposta.status(200).json(dados.rfid) // [0] é a coluna q vai ficar o RFID
         }
     },
+    /**
+        * Lida com requisição GET respondendo com um vetor com todas as entradas ordenadas de mais recente pra menos recente
+        * @memberof pyControladora
+        * @method listarTodos
+        * @param {Object} requisicao Parametro padrão e fornecido pelo Express, guarda as informações da requisição como corpo e o tipo
+        * @param {Object} resposta Parametro padrão e fornecido pelo Express, guarda as informações da resposta como o corpo e o status
+        * @returns O retorno nessa função é desnecessário e é feito só para não gerar confusão quanto ao fim da função, o que importa é a chamada dos metodos do obejto "resposta"
+    */
     listarTodos: async function (requisicao, resposta){
         const dados = rfidRepositorio.buscarTodos()
         return resposta.status(200).json(dados)
