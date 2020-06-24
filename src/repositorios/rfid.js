3798042c9aa6a55a5ad49807dd9331a41d54ee2a
4000napa <beto.lhcs@gmail.com>
2020-06-23 21:45:33 -0300

    comentei uma parte do ćodigo experimental

--- src/repositorios/rfid.js
+++ src/repositorios/rfid.js
@@ -47,48 +47,48 @@ const rfid = {
             historico.rows[i].data = datasFormatadas.rows[i].to_char
         }
         return historico.rows
-    },
-    relatorioMensal: async function(){ //ou semanal sei la
-        var doc = new jsPDF();
+    }//,
+    // relatorioMensal: async function(){ //ou semanal sei la
+    //     var doc = new jsPDF();
 
-        var tabela = document.getElementById(tabelaID);
-        var hoje = new Date();
-        var dd = hoje.getDate();
-        var mm = hoje.getMonth() + 1;
-        var yyyy = hoje.getFullYear();
-        if (dd < 10) dd = '0' + dd;
-        if (mm < 10) mm = '0' + mm;
-        hoje = dd + '-' + mm + '-' + yyyy
-        var nome = tabelaID + '-' + hoje;
-        //cabeçalho
+    //     var tabela = document.getElementById(tabelaID);
+    //     var hoje = new Date();
+    //     var dd = hoje.getDate();
+    //     var mm = hoje.getMonth() + 1;
+    //     var yyyy = hoje.getFullYear();
+    //     if (dd < 10) dd = '0' + dd;
+    //     if (mm < 10) mm = '0' + mm;
+    //     hoje = dd + '-' + mm + '-' + yyyy
+    //     var nome = tabelaID + '-' + hoje;
+    //     //cabeçalho
 
-        var header = function (data) {
-            var img = new Image()
-            if (window.location.href.indexOf('relatorios') >= 0) {
-                img.src = ('../assets/mecajun_logo_pdf.png');
-            } 
-            else {
-                img.src = ('./assets/mecajun_logo_pdf.png');
-            }
-            var imgOffset = (doc.internal.pageSize.width - 50) / 2;
-            doc.addImage(img, 'png', imgOffset, 20, 50, 25);
-        }
-        // Título do Documento
+    //     var header = function (data) {
+    //         var img = new Image()
+    //         if (window.location.href.indexOf('relatorios') >= 0) {
+    //             img.src = ('../assets/mecajun_logo_pdf.png');
+    //         } 
+    //         else {
+    //             img.src = ('./assets/mecajun_logo_pdf.png');
+    //         }
+    //         var imgOffset = (doc.internal.pageSize.width - 50) / 2;
+    //         doc.addImage(img, 'png', imgOffset, 20, 50, 25);
+    //     }
+    //     // Título do Documento
 
-        doc.setFont('Times New Roman');
-        doc.setFontSize(18);
-        doc.setTextColor(24, 109, 105);
-        doc.setFontType("bold");
-        var hoje_titulo = dd + '/' + mm + '/' + yyyy
-        var titulo = tabela.childNodes[1].innerHTML + ' de ' + tabela.childNodes[1].id + ' até ' + hoje_titulo
-        var textWidth = doc.getStringUnitWidth(titulo) * doc.internal.getFontSize() / doc.internal.scaleFactor;
-        var textOffset = (doc.internal.pageSize.width - textWidth) / 2;
-        doc.text(textOffset, 52, titulo)
-        // Tabela
+    //     doc.setFont('Times New Roman');
+    //     doc.setFontSize(18);
+    //     doc.setTextColor(24, 109, 105);
+    //     doc.setFontType("bold");
+    //     var hoje_titulo = dd + '/' + mm + '/' + yyyy
+    //     var titulo = tabela.childNodes[1].innerHTML + ' de ' + tabela.childNodes[1].id + ' até ' + hoje_titulo
+    //     var textWidth = doc.getStringUnitWidth(titulo) * doc.internal.getFontSize() / doc.internal.scaleFactor;
+    //     var textOffset = (doc.internal.pageSize.width - textWidth) / 2;
+    //     doc.text(textOffset, 52, titulo)
+    //     // Tabela
 
-        doc.autoTable({ didDrawPage: header, margin: { top: 57 }, html: '#' + tabela.childNodes[3].id })
-        doc.save(nome + '.pdf');
-    } //Pra n ficar guardando dados desnecessários
+    //     doc.autoTable({ didDrawPage: header, margin: { top: 57 }, html: '#' + tabela.childNodes[3].id })
+    //     doc.save(nome + '.pdf');
+    // } //Pra n ficar guardando dados desnecessários
 }
 
 module.exports = rfid
